d1003e3792181a2a18ed276393a0b057d1db7484
Joans-sebastian <joans.sebastianv@uqvirtual.edu.co>
2024-09-29 00:52:57 -0500

    Update App.java

--- biblioteca2/biblioteca/src/main/java/co/edu/uniquindio/poo/App.java
+++ biblioteca2/biblioteca/src/main/java/co/edu/uniquindio/poo/App.java
@@ -1,11 +1,40 @@
 package co.edu.uniquindio.poo;
 
-/**
- * Hello world!
- *
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Crear listas de objetos
+        List<Bibliotecario> bibliotecarios = new ArrayList<>();
+        List<Prestamo> prestamos = new ArrayList<>();
+        List<Libro> libros = new ArrayList<>();
+        List<Estudiante> estudiantes = new ArrayList<>();
+        List<DetallePrestamo> detallePrestamos = new ArrayList<>();
+
+        // Crear una Biblioteca
+        Biblioteca biblioteca = new Biblioteca("Biblioteca Central", 100000.0, bibliotecarios, prestamos, libros, estudiantes, detallePrestamos);
+
+        // Imprimir el nombre de la biblioteca (ejemplo de uso)
+        System.out.println("Nombre de la Biblioteca: " + biblioteca.getNombre());
+
+         // Crear un bibliotecario y agregarlo a la lista
+         Bibliotecario bibliotecario1 = new Bibliotecario("brayan", "13579", "brayan@gmail.com", 111111111, 1000000.0,  LocalDate.of(2023, 9, 28));
+         bibliotecarios.add(bibliotecario1);
+ 
+         // Crear un estudiante y agregarlo a la lista
+         Estudiante estudiante1 = new Estudiante("joan", "2468", "joan@gmail.com", 222222222, false);
+         estudiantes.add(estudiante1);
+ 
+         
+         // Imprimir información del estudiante
+         System.out.println("Estudiante: " + estudiante1.getNombre()); 
+         // Asegúrate de tener un método getNombre en Estudiante
+
+         // Imprimir información del bibliotecario
+        System.out.println("Bibliotecario: " + bibliotecario1.getNombre()); 
+        // Asegúrate de tener un método getFechaContratacion en Bibliotecario
+
     }
 }
